65650a23819d2c7c4f85febeaf12e78701017759
KaitlynKeil <kaitlyn.keil@students.olin.edu>
2016-11-30 11:25:06 -0500

    Create sprint_3_python.py
    The incomplete python control code

---
+++ sprint_3_python.py
@@ -0,0 +1,158 @@
+import time
+
+class Turtle(object):
+    """ Creates the overarching Turtle class, which controlls the turtle """
+
+    def __init__(self):
+        """ Imports serial and creates the arduino """
+        aSD = serial.Serial('/dev/ttyACM0', 9600) # aSD: arduinoSerialData, check port number
+
+        # Initialize all the sensors
+        self.LS = {'front':0, 'back':0} # Light sensor dictionary
+        self.IR = {'front':0, 'right':0, 'left':0} # IR sensor dictionary
+        self.TS = 0 # Temperature sensor
+        self.SM = 0 # Soil moisture sensor
+
+        # Set up limits to look for (filler values)
+        self.distanceLimit = 100 # limit before something is 'seen' in that direction
+        self.seeLight = 100
+        self.heatLimit = 80 # if temperature sensor is above this value, look for shade
+        self.moistureLimit = 100 # if moisture is too low, change eye color
+        
+        # Set up decision points to control behavior
+        self.seekShade = False
+        self.inLight = 0 # 0 indicates fully in light, 1 is front only, 2 is back only, 3 is full shade
+        self.lastLightUpdate = time.time() # record the time spent in light every minute
+        self.minuteLightAverage = 0 # averages the light received over a minute
+        self.timeSpentInLight = 0 # keeps track of how long we've been in the light 
+        self.HLQ = {'day':60, 'night':0, 'met':False} # if it is daytime, try to get light for 8 hours. If night, no light needed
+        self.timeStarted = time.time() # will let us figure out if it is day or night
+        self.currentHour = 0
+
+    def run(self):
+        """ Loop that runs the decision algorithm """
+        while True:
+            if(aSD.inWaiting()&gt;0):
+                dataRead = aSD.readline()
+                self.assignData(dataRead)
+
+                # Check to see if we need to go to shade
+                enoughLight = self.checkHourlyLight()
+                tooHot = self.TS > self.heatLimit
+
+                if enoughLight or tooHot:
+                    self.seekShade = True
+                else:
+                    self.seekShade = False
+
+                if self.seekShade:
+                    arduinoSerialData.write(self.goToShade())
+                else:
+                    arduinoSerialData.write(self.goToLight())
+
+                if self.SM < self.moistureLimit:
+                    eyesYellow = True
+                else:
+                    eyesYellow = False
+
+    def assignData(self, dataRead):
+        """ Assigns the data to the proper variables and converts it to floats.
+            Data is received as: 'FLS,BLS,FIR,RIR,LIR,TS,SM'. """
+        dataList = dataRead.split(',')
+        for index in range(len(dataList)):
+            dataList[index] = float(dataList[index])
+        self.LS['front'], self.LS['back'] = dataList[0], dataList[1]
+        self.IR['front'], self.IR['right'], self.IR['left'] = dataList[2], dataList[3], dataList[4]
+        self.TS = dataList[5]
+        self.SM = dataList[6]
+
+    def checkInLight(self):
+        """ Checks if the turtle is in light.
+            0 means fully in light, 1 means the back is in shadow,
+            2 means the front is in shadow and the back isn't,
+            3 means fully in shadow. """
+        if self.LS['front'] >= self.seeLight and self.LS['back'] >= self.seeLight:
+            self.inLight = 0
+        elif self.LS['front'] >= self.seeLight and self.LS['back'] < self.seeLight:
+            self.inLight = 1
+        elif self.LS['front'] < self.seeLight and self.LS['back'] >= self.seeLight:
+            self.inLight = 2
+        else:
+            self.inLight = 3
+        
+    def checkHourlyLight(self):
+        """ Checks to see how much light has been received in the hour.
+            Returns True if the quota has been met, False if it has not. """
+        # Check if it has been an hour since currentHour was last updated
+        hourCheck = int((self.timeStarted - time.time())/3600)
+        if hourCheck != self.currentHour:
+            # if it has been an hour since these were zeroed out, zero 'em off
+           self.minuteLightAverage = 0
+           self.self.lastLightUpdated = time.time()
+           self.timeSpentInLight = 0
+           self.HLQ['met'] = False
+           self.currentHour = hourCheck
+
+        self.checkInLight()
+
+        # add different amounts depending on how much light the turtle is in
+        if self.inLight == 0:
+            self.minuteLightAverage += 1
+        elif self.inLight == 1 or self.inLight == 2:
+            self.minuteLightAverage += .5
+        else:
+            self.minuteLightAverage += 0
+
+        # If it has been a minute since the last average was taken, take it
+        if(self.lastLightUpdate - time.time()) >= 60: # if it has been 1 minute, update the timeSpentInLight
+            self.timeSpentInLight += self.minuteLightAverage/60 # assuming one check per second
+            self.minuteLightAverage = 0
+            self.lastLightUpdate = time.time()
+
+        if self.currentHour < 8:
+            lightQuota = self.HLQ['day']
+        else:
+            lightQuota = self.HLQ['night']
+
+        if lightQuota < self.timeSpentInLight:
+            self.HLQ['met'] = True
+            return self.HLQ
+        else:
+            return self.HLQ
+
+    def goToShade(self):
+        """ Travels to the shade and stops if fully in shade.
+            Returns 'forward', 'right', 'left', or 'stop' """
+        if self.inLight == 3: # stop if in shade
+            return 'stop'
+        elif self.inLight == 1: # try to turn around if there is shade behind
+            return 'right'
+        else:
+            return self.checkForward()
+
+    def goToLight(self):
+        """ Wanders around in the light, turns if it gets into shade. """
+        if self.inLight == 2:
+            # turn if the front IR gets into the shade
+            return self.checkTurns()
+        else:
+            # otherwise, go wherever
+            return self.checkForward()
+        
+    def checkFoward(self):
+        """ Checks to see which way it can go and returns 'forward', 'right', or 'left' """
+        if self.IR['front'] < self.distanceLimit:
+            return 'forward'
+        else: # check which direction we should turn
+            return checkTurns()
+
+    def checkTurns(self):
+        """ Checks which turn it should make. Returns 'right' or 'left' """
+        if self.IR['right'] < self.distanceLimit and (self.IR['right'] - self.IR['left']) < 25:
+            # checks to see if there is anything to the right. If there is not,
+            #  and there is nothing significantly closer to the right than the left,
+            #  the turtle turns right
+            return 'right'
+        else:
+            # goes left if it can't go forward or right. Maybe change later?
+            return 'left'
